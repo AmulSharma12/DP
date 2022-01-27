c2c89cd278e2cc3d3069b95b2af10bd5522531a3
Amul Op <amulsharmarr67@gmail.com>
2022-01-27 16:30:32 +0530

    shortestcommon-supersequence

--- AdiyaVerma/L24_ShortestCommonSuperSequence.java
+++ AdiyaVerma/L24_ShortestCommonSuperSequence.java
@@ -4,6 +4,27 @@ public class L24_ShortestCommonSuperSequence
 {
 	public static void main(String[] args)
 	{
-		
+		String a = "geek";
+		String b = "eke";
+		int m = a.length();
+		int n = b.length();
+
+
+		int result  = m+n -lcs(a, b, m, n);
+		System.out.println(result);
+	}
+
+
+	//lcs method
+	public static int lcs(String x, String y, int m, int n)
+	{
+		if(m == 0 || n == 0)
+			return 0;
+
+
+		if(x.charAt(m-1) == y.charAt(n-1))
+			return 1 + lcs(x, y, m-1, n-1);
+
+		return Math.max(lcs(x, y, m, n-1) , lcs(x, y, m-1, n));
 	}
 }
